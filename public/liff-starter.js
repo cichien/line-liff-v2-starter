33ff93c0ab246710d6261e7e143ab008c2b6bd23
Chien Cheng Yi <coke.chien@linecorp.com>
2020-10-19 13:38:33 +0800

    test geo location

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    navigator.geolocation.getCurrentPosition(console.log, console.log);
     const useNodeJS = true;   // if you are not using a node server, set this value to false
     const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
 
